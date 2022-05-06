c3129c4a489722e7620860a5b2c95f6b1ec30652
zomal <tehne@tlen.pl>
2022-05-06 19:42:46 +0200

    third commit wiht api

--- src/main/java/pl/keep/javac/simplespringbootapp/PatientsController.java
+++ src/main/java/pl/keep/javac/simplespringbootapp/PatientsController.java
@@ -3,9 +3,12 @@ package pl.keep.javac.simplespringbootapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -27,10 +30,26 @@ public class PatientsController {
         }else {
             countr=1;
         }
-
+        String sids = session.getId();
         model.addAttribute("patients",patientList);
         model.addAttribute("avgPrice",avgPrice);
         session.setAttribute(COUN,countr);
+        session.setAttribute("seid",sids);
+        Long ct = session.getCreationTime();
+        session.setAttribute("lct",ct);
         return "index";
     }
+
+    @GetMapping("/api/patients")
+    @ResponseBody
+    List<Patient>getAllPatients(){
+        List<Patient> patients = new ArrayList<>();
+        patients.add(new Patient(1L,"tom","tatars",new BigDecimal("345")));
+        patients.add(new Patient(2L,"jon","puczyk",new BigDecimal("456")));
+        patients.add(new Patient(3L,"asz","walusz",new BigDecimal("567")));
+        patients.add(new Patient(4L,"lui","kozior",new BigDecimal("789")));
+
+        return patients;
+    }
+
 }
