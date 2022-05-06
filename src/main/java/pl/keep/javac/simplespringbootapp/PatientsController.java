91d173d23dc5ae33020df49c280052ea8fbfbf3b
zomal <tehne@tlen.pl>
2022-05-06 10:14:11 +0200

    second msg with session value

--- src/main/java/pl/keep/javac/simplespringbootapp/PatientsController.java
+++ src/main/java/pl/keep/javac/simplespringbootapp/PatientsController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,16 +12,25 @@ import java.util.List;
 public class PatientsController {
 
     private final PatientRepository patientRepository;
-
+    private static final String COUN = "lw";
     public PatientsController(PatientRepository patientRepository1){
         this.patientRepository = patientRepository1;
     }
     @GetMapping("/")
-    String home (Model model){
+    String home (Model model,HttpSession session){
+
         List<Patient> patientList = patientRepository.showAll();
         BigDecimal avgPrice = patientRepository.getAvgPrice();
+        Integer countr = (Integer)session.getAttribute(COUN);
+        if (countr != null){
+            countr++;
+        }else {
+            countr=1;
+        }
+
         model.addAttribute("patients",patientList);
         model.addAttribute("avgPrice",avgPrice);
+        session.setAttribute(COUN,countr);
         return "index";
     }
 }
